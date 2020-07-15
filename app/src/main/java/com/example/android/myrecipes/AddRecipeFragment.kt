8c245c94ec783c2d13c984db83a8e3b614fbbe70
Aparna Pattathil <aparna.pattathil@lastminute.com>
2020-07-15 13:48:14 +0200

    add fragment to add recipes

---
+++ app/src/main/java/com/example/android/myrecipes/AddRecipeFragment.kt
@@ -0,0 +1,25 @@
+package com.example.android.myrecipes
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+
+class AddRecipeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_add_recipe, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.save_menu, menu)
+    }
+}
