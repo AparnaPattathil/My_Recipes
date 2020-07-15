86f75f05a80dd91c3c18cbd7031ae4216f8a4f1d
Aparna Pattathil <aparna.pattathil@lastminute.com>
2020-07-15 11:56:12 +0200

    add recycler view adapter

---
+++ app/src/main/java/com/example/android/myrecipes/RecipesAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.android.myrecipes
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.myrecipes.databinding.RecipeDetailsItemBinding
+
+class RecipesAdapter(private val recipes: List<Recipe>) :
+    RecyclerView.Adapter<RecipesAdapter.RecipesViewHolder>() {
+
+    override fun getItemCount(): Int = recipes.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        RecipesViewHolder(
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                R.layout.recipe_details_item,
+                parent,
+                false
+            )
+        )
+
+    override fun onBindViewHolder(holder: RecipesViewHolder, position: Int) {
+        holder.recipeDetailsItemBinding.recipe = recipes[position]
+    }
+
+    inner class RecipesViewHolder(
+        val recipeDetailsItemBinding: RecipeDetailsItemBinding
+    ) : RecyclerView.ViewHolder(recipeDetailsItemBinding.root) {
+    }
+}
