8d2625c06f39d8db9d036754e5cbff80ca44eabb
Aparna Pattathil <aparna.pattathil@lastminute.com>
2020-07-15 12:22:12 +0200

    add recycler view adapter to fragment

--- app/src/main/java/com/example/android/myrecipes/AllRecipesFragment.kt
+++ app/src/main/java/com/example/android/myrecipes/AllRecipesFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.all_recipes_fragment.*
 
 class AllRecipesFragment : Fragment() {
 
@@ -23,6 +26,15 @@ class AllRecipesFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         val repository = RecipesRepository()
         factory = AllRecipesViewModelFactory(repository)
-        viewModel = ViewModelProvider(this).get(AllRecipesViewModel::class.java)
+        viewModel = ViewModelProviders.of(this, factory).get(AllRecipesViewModel::class.java)
+        viewModel.fetchAllRecipes()
+        viewModel.recipes.observe(viewLifecycleOwner, Observer { recipes ->
+            recycler_view_recipes.also {
+                it.layoutManager = LinearLayoutManager(requireContext())
+                it.setHasFixedSize(true)
+                it.adapter = RecipesAdapter(recipes)
+            }
+
+        })
     }
 }
