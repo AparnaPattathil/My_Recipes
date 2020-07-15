7b28368f8ba55583a13e7fe0252f1d9df6db9177
Aparna Pattathil <aparna.pattathil@lastminute.com>
2020-07-15 08:36:50 +0200

    fetch recipes in different threads

--- app/src/main/java/com/example/android/myrecipes/AllRecipesViewModel.kt
+++ app/src/main/java/com/example/android/myrecipes/AllRecipesViewModel.kt
@@ -3,16 +3,28 @@ package com.example.android.myrecipes
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.Job
 
 class AllRecipesViewModel(private val repository: RecipesRepository) : ViewModel() {
+
+    private lateinit var job: Job
 
     private val _recipes = MutableLiveData<List<Recipe>>()
 
     val recipes: LiveData<List<Recipe>>
         get() = _recipes
 
-    suspend fun fetchAllRecipes() {
-        val recipes = repository.fetchAllRecipes()
-        _recipes.value = recipes
+    fun fetchAllRecipes() {
+        job = Coroutines.ioThenMain(
+            { repository.fetchAllRecipes() },
+            {
+                _recipes.value = it
+            }
+        )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        if (::job.isInitialized) job.cancel()
     }
 }
