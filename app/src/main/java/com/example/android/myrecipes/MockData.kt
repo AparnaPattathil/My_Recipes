4736f837c173ad354681662fb7038873de5366e1
Aparna Pattathil <aparna.pattathil@lastminute.com>
2020-07-16 22:11:49 +0200

    add mock data

---
+++ app/src/main/java/com/example/android/myrecipes/MockData.kt
@@ -0,0 +1,42 @@
+package com.example.android.myrecipes
+
+class MockData {
+    var recipes = listOf(
+        Recipe(
+            "Green Chicken",
+            "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+
+
+            ),
+        Recipe(
+            "Green Chicken",
+            "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+
+        ),
+        Recipe(
+            "Green Chicken",
+            "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+
+        ),
+        Recipe(
+            "Green Chicken",
+            "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+
+        ),
+        Recipe(
+            "Green Chicken",
+            "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+
+        ),
+        Recipe(
+            "Green Chicken",
+            "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+
+        ),
+        Recipe(
+            "Green Chicken",
+            "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+
+        )
+    )
+}
